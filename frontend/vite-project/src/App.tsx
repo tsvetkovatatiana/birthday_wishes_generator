fc213973f57243241e33d61e28df9cf7907a736f
Tatiana Tsvetkova <137390908+tsvetkovatatiana@users.noreply.github.com>
2025-04-29 13:20:04 +0300

    initialising App

---
+++ frontend/vite-project/src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header.tsx"
+import WishForm from "./components/WishForm.tsx";
+import WelcomeBox from "./components/WelcomeBox.tsx";
+
+export default function App() {
+
+    return (
+        <>
+            <Header />
+            <WelcomeBox />
+            <WishForm />
+        </>
+    )
+}
