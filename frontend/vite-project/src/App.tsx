67755140093b080848a6d52cdbad88f27bdd9125
Tatiana Tsvetkova <137390908+tsvetkovatatiana@users.noreply.github.com>
2025-04-29 17:13:23 +0300

    add all components such as box with instructions, form for user's input and box with model's response to one file.

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,13 +1,11 @@
 import Header from "./components/Header.tsx"
 import WishForm from "./components/WishForm.tsx";
-import WelcomeBox from "./components/WelcomeBox.tsx";
 
 export default function App() {
 
     return (
         <>
             <Header />
-            <WelcomeBox />
             <WishForm />
         </>
     )
